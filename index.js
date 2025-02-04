0e89f3e43c21f55bb65f52084ec807cea0cb6e1b
santhoshT15 <ramsanthoshbms@gmail.com>
2025-02-05 05:09:50 +0530

    frontend link change

--- index.js
+++ index.js
@@ -38,17 +38,17 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://santhosh-travel-log-app.netlify.app/",
     credentials: true,
   })
 );
