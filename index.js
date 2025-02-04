1e8956d0f768112e985a45f1a6db0079929e52d6
santhoshT15 <ramsanthoshbms@gmail.com>
2025-02-05 04:21:14 +0530

    cors update

--- index.js
+++ index.js
@@ -39,12 +39,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 //app.use(morgan("common"));
 
