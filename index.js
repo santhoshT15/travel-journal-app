52eced784cd60baa297ecb08578647ffe6fd82b1
santhoshT15 <ramsanthoshbms@gmail.com>
2025-02-12 19:00:56 +0530

    cors commit

--- index.js
+++ index.js
@@ -39,7 +39,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://santhosh-travel-log-app.netlify.app/"
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
